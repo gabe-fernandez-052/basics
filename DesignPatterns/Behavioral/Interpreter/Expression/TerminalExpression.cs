23d8f21b2098283ecabe4be4009343c0295cfc8e
Gabe Fernandez <gabriel052.gf@gmail.com>
2026-05-25 12:02:50 -0400

    Replace with book example

--- DesignPatterns/Behavioral/Interpreter/Expression/TerminalExpression.cs
+++
@@ -1,9 +0,0 @@
-namespace DesignPatterns.Behavioral.Interpreter.Expression
-{
-    public class TerminalExpression(string value) : IExpression
-    {
-        private readonly string _value = value;
-
-        public bool Interpret(HashSet<string> context) => context.Contains(_value);
-    }
-}
