2973c8e8d925b4cfe5f6125fcab70e642380cd16
Gabe Fernandez <gabriel052.gf@gmail.com>
2026-05-25 12:05:25 -0400

    Update to book example

--- DesignPatterns/Behavioral/State/Client.cs
+++ DesignPatterns/Behavioral/State/Client.cs
@@ -1,33 +1,36 @@
-using DesignPatterns.Behavioral.State.Context;
-
 namespace DesignPatterns.Behavioral.State
 {
     /// <summary>
     /// What is it?
     /// Allow an object to alter its behavior when its internal state changes. The object will appear to change its class.
     /// When to use?
-    /// An object's behavior depends on its state, and it must change its behavior at run-time depending on that state.
+    /// An object's behavior depends on its state, and it must change its behavior at run time depending on that state.
     /// Operations have large, multipart conditional statements that depend on the object's state.
-    /// The State pattern puts each branch of the conditional in a separate class.
     /// </summary>
     public class Client
     {
         public void Run()
         {
-            var vendingMachine = new VendingMachine();
+            var connection = new TcpConnection();
 
-            vendingMachine.InsertMoney();
-            vendingMachine.SelectProduct();
-            vendingMachine.Dispense();
+            Console.WriteLine("Opening connection:");
+            connection.Open();
 
             Console.WriteLine();
+            Console.WriteLine("Acknowledging (completing handshake):");
+            connection.Acknowledge();
 
-            // Try invalid transitions
-            vendingMachine.SelectProduct();
-            vendingMachine.InsertMoney();
-            vendingMachine.InsertMoney();
-            vendingMachine.SelectProduct();
-            vendingMachine.Dispense();
+            Console.WriteLine();
+            Console.WriteLine("Acknowledging data:");
+            connection.Acknowledge();
+
+            Console.WriteLine();
+            Console.WriteLine("Closing connection:");
+            connection.Close();
+
+            Console.WriteLine();
+            Console.WriteLine("Trying to close again:");
+            connection.Close();
         }
     }
 }
