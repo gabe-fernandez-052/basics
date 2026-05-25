611ad2ae434c60a95b0850ca400e9095bbb87f8a
Gabe Fernandez <gabriel052.gf@gmail.com>
2026-05-25 11:40:43 -0400

    Add design pattern

---
+++ DesignPatterns/Behavioral/Interpreter/Expression/AndExpression.cs
@@ -0,0 +1,11 @@
+namespace DesignPatterns.Behavioral.Interpreter.Expression
+{
+    public class AndExpression(IExpression left, IExpression right) : IExpression
+    {
+        private readonly IExpression _left = left;
+        private readonly IExpression _right = right;
+
+        public bool Interpret(HashSet<string> context) =>
+            _left.Interpret(context) && _right.Interpret(context);
+    }
+}
