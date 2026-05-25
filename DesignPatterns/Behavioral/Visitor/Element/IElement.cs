c3379ef11a0a74487db5a9c73c2d05cb46a13a68
Gabe Fernandez <gabriel052.gf@gmail.com>
2026-05-25 12:06:15 -0400

    Replace with book example

--- DesignPatterns/Behavioral/Visitor/Element/IElement.cs
+++
@@ -1,9 +0,0 @@
-using DesignPatterns.Behavioral.Visitor.Visitor;
-
-namespace DesignPatterns.Behavioral.Visitor.Element
-{
-    public interface IElement
-    {
-        void Accept(IVisitor visitor);
-    }
-}
